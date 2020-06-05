9f5961342a2f4555f435ad8192ca5690a1e4d733
Giovanni Calò <giovanni.calo@live.it>
2020-06-05 01:06:17 +0100

    Improved behavior when colors are not supported

--- source/enhance.js
+++ source/enhance.js
@@ -8,14 +8,20 @@ const prettyError = new PrettyError();
 module.exports = (values) => {
 	return (match, placeholder) => {
 		const value = values[placeholder];
-		if (value instanceof Error) {
-			return prettyError.render(value);
-		} else if (typeof value === "string") {
-			return chalk.green(value);
+		if (chalk.level) {
+			if (value instanceof Error) {
+				return prettyError.render(value);
+			} else if (typeof value === "string") {
+				return chalk.green(value);
+			} else {
+				return inspect(value, { colors: true });
+			}
+		} else if (value instanceof Error) {
+			return value.stack;
+		} else if (typeof value === "object") {
+			return JSON.stringify(value, null, "\t");
 		} else {
-			return inspect(value, {
-				colors: true
-			});
+			return value;
 		}
 	};
 };

7dac145db6ef31a724db48aeb9e24aeec6c72599
Giovanni Calò <giovanni.calo@live.it>
2020-08-22 00:50:25 +0100

    Improved error message when there is no stack

--- source/enhance/error.js
+++ source/enhance/error.js
@@ -7,40 +7,43 @@ const findRoot = require("./find-root");
 const rootDirectory = findRoot(require.main.filename);
 
 const enhanceError = ({ message, name, stack }) => {
+	const enhancedStack = stack.split("\n").slice(1).reduce((trace, item, index) => {
+		const match = item.match(
+			/^\s*at\s(?:(?<method>.+)\s\()?(?:(?<path>.+):(?<line>\d+):(?<column>\d+))?\)?$/u
+		);
+		if (match) {
+			const { groups: { column, line, method, path } } = match;
+			if (path && existsSync(path)) {
+				const code = readFileSync(path, "utf-8").split("\n").slice(line - 1, line)[0]; // TODO: Make asynchronous
+				return `${
+					trace
+				}${
+					index ? "\n" : ""
+				}  ${
+					method || "<unknown>"
+				}\n  ${
+					chalk.gray(path.replace(rootDirectory, "").replace(/\\/gu, "/"))
+				}:${
+					chalk.yellowBright(line)
+				}:${
+					chalk.yellowBright(column)
+				}\n  ${
+					chalk.redBright(code.replace(/^\s*/u, ""))
+				}\n  ${
+					chalk.cyanBright(new Array(column - code.match(/^\s*/u)[0].length - 1).fill(" ").concat(["^"]).join(""))
+				}`;
+			}
+		}
+		return trace;
+	}, "");
 	return `${
 		chalk.yellowBright(name)
 	}: ${
 		message
-	}\n${
-		stack.split("\n").slice(1).reduce((trace, item, index) => {
-			const match = item.match(
-				/^\s*at\s(?:(?<method>.+)\s\()?(?:(?<path>.+):(?<line>\d+):(?<column>\d+))?\)?$/u
-			);
-			if (match) {
-				const { groups: { column, line, method, path } } = match;
-				if (path && existsSync(path)) {
-					const code = readFileSync(path, "utf-8").split("\n").slice(line - 1, line)[0]; // TODO: Make asynchronous
-					return `${
-						trace
-					}${
-						index ? "\n" : ""
-					}  ${
-						method || "<unknown>"
-					}\n  ${
-						chalk.gray(path.replace(rootDirectory, "").replace(/\\/gu, "/"))
-					}:${
-						chalk.yellowBright(line)
-					}:${
-						chalk.yellowBright(column)
-					}\n  ${
-						chalk.redBright(code.replace(/^\s*/u, ""))
-					}\n  ${
-						chalk.cyanBright(new Array(column - code.match(/^\s*/u)[0].length - 1).fill(" ").concat(["^"]).join(""))
-					}`;
-				}
-			}
-			return trace;
-		}, "")
+	}${
+		enhancedStack ? `\n${
+			enhancedStack
+		}` : ""
 	}`;
 };
 

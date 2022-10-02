67dfc88bbcfb29f0e1cd30faae9c2d87122dd544
Giovanni Calò <giovanni.calo@live.it>
2022-10-02 23:06:22 +0100

    Improved types

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,26 +1,32 @@
-/**
- * Logs an error message.
- *
- * @param {String[]} strings - Strings to interleave.
- * @param {...*} values - Values to interleave.
- * @returns {void}
- */
-export function error(strings: string[], ...values: any): void
+declare namespace log {
 
-/**
- * Logs an info message.
- *
- * @param {String[]} strings - Strings to interleave.
- * @param {...*} values - Values to interleave.
- * @returns {void}
- */
-export function info(strings: string[], ...values: any): void
+	/**
+	 * Logs an error message.
+	 *
+	 * @param {String[]} strings - Strings to interleave.
+	 * @param {...*} values - Values to interleave.
+	 * @returns {void}
+	 */
+	function error(strings: string[], ...values: any): void;
 
-/**
- * Logs a warning message.
- *
- * @param {String[]} strings - Strings to interleave.
- * @param {...*} values - Values to interleave.
- * @returns {void}
- */
-export function warning(strings: string[], ...values: any): void
+	/**
+	 * Logs an info message.
+	 *
+	 * @param {String[]} strings - Strings to interleave.
+	 * @param {...*} values - Values to interleave.
+	 * @returns {void}
+	 */
+	function info(strings: string[], ...values: any): void;
+
+	/**
+	 * Logs a warning message.
+	 *
+	 * @param {String[]} strings - Strings to interleave.
+	 * @param {...*} values - Values to interleave.
+	 * @returns {void}
+	 */
+	function warning(strings: string[], ...values: any): void;
+
+}
+
+export = log;
